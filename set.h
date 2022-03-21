2395c8ef38d01ebd3e12315f7cef1d573ecbfc81
shishyando <68201550+shishyando@users.noreply.github.com>
2022-03-21 15:16:22 +0300

    Overall clean-up

--- set.h
+++ set.h
@@ -1,50 +1,44 @@
+#include <algorithm>
 #include <cstddef>
 #include <initializer_list>
-#include <algorithm>
 #include <iostream>
 
+// Effective std::set analogue based on AVL tree.
 template<class T>
 class Set {
-
-public:
-    class Node;
-
-    class iterator;
-
+  public:
     class Node {
-    public:
-        Node() : val() {
-            L = R = nullptr;
+      public:
+        Node() : value() {
+            left = right = nullptr;
             next = prev = nullptr;
-            h = 0;
+            height = 0;
         }
 
-        explicit Node(const T &elem) : val(elem) {
-            L = R = nullptr;
+        explicit Node(const T& elem) : value(elem) {
+            left = right = nullptr;
             next = prev = nullptr;
-            h = 1;
+            height = 1;
         }
 
-        T val;
-        Node *L = nullptr, *R = nullptr;
+        T value;
+        Node *left = nullptr, *right = nullptr;
         Node *next = nullptr, *prev = nullptr;
-        int h = 0;
+        size_t height = 0;
     };
 
     class iterator {
-    public:
-        iterator() : node_(nullptr) {
-        }
+      public:
+        iterator() : node_(nullptr) {}
 
-        explicit iterator(const Node *node) : node_(node) {
-        }
+        explicit iterator(const Node* node) : node_(node) {}
 
-        iterator &operator++() {
+        iterator& operator++() {
             node_ = node_->next;
             return *this;
         }
 
-        iterator &operator--() {
+        iterator& operator--() {
             node_ = node_->prev;
             return *this;
         }
@@ -61,38 +55,29 @@ public:
             return copied;
         }
 
-        const T &operator*() const {
-            return node_->val;
-        }
+        const T& operator*() const { return node_->value; }
 
-        const T *operator->() const {
-            return &node_->val;
-        }
+        const T* operator->() const { return &node_->value; }
 
-        bool operator!=(const iterator it) const {
-            return node_ != it.node_;
-        }
+        bool operator!=(const iterator it) const { return node_ != it.node_; }
 
-        bool operator==(const iterator it) const {
-            return node_ == it.node_;
-        }
+        bool operator==(const iterator it) const { return node_ == it.node_; }
 
-    private:
-        const Node *node_;
+      private:
+        const Node* node_;
     };
 
-
-public:
+  public:
     Set() {
         size_ = 0;
         root_ = min_ = max_ = nullptr;
-        im_ = new Node();
+        imaginary_ = new Node();  // An auxiliary node to maintain valid end().
     }
 
-    Set(const Set &other) {
+    Set(const Set& other) {
         root_ = deep_copy(other.root_);
         size_ = other.size_;
-        std::vector<Node *> temp;
+        std::vector<Node*> temp;
         fix_prev_next(root_, temp);
         for (size_t i = 1; i < size_; ++i) {
             temp[i - 1]->next = temp[i];
@@ -101,22 +86,22 @@ public:
         temp.clear();
         min_ = get_min(root_);
         max_ = get_max(root_);
-        im_ = new Node();
-        im_->prev = max_;
+        imaginary_ = new Node();
+        imaginary_->prev = max_;
         if (max_ != nullptr) {
-            max_->next = im_;
+            max_->next = imaginary_;
         }
     }
 
-    Set &operator=(const Set &other) {
+    Set& operator=(const Set& other) {
         if (&other == this) {
             return *this;
         }
         del(root_);
-        delete im_;
+        delete imaginary_;
         root_ = deep_copy(other.root_);
         size_ = other.size_;
-        std::vector<Node *> temp;
+        std::vector<Node*> temp;
         fix_prev_next(root_, temp);
         for (size_t i = 1; i < size_; ++i) {
             temp[i - 1]->next = temp[i];
@@ -125,10 +110,10 @@ public:
         temp.clear();
         min_ = get_min(root_);
         max_ = get_max(root_);
-        im_ = new Node();
-        im_->prev = max_;
+        imaginary_ = new Node();
+        imaginary_->prev = max_;
         if (max_ != nullptr) {
-            max_->next = im_;
+            max_->next = imaginary_;
         }
         return *this;
     }
@@ -136,45 +121,44 @@ public:
     template<typename Iterator>
     Set(Iterator first, Iterator last) {
         size_ = 0;
-        im_ = new Node();
+        imaginary_ = new Node();
         for (auto it = first; it != last; ++it) {
             insert(*it);
         }
     }
 
-    Set(std::initializer_list<T> &elems) {
+    Set(std::initializer_list<T>& elems) {
         size_ = 0;
-        im_ = new Node();
-        for (auto elem: elems) {
+        imaginary_ = new Node();
+        for (auto elem : elems) {
             insert(elem);
         }
     }
 
     Set(std::initializer_list<T> elems) {
         size_ = 0;
-        im_ = new Node();
-        for (auto elem: elems) {
+        imaginary_ = new Node();
+        for (auto elem : elems) {
             insert(elem);
         }
     }
 
-    size_t size() const {
-        return size_;
-    }
+    // Returns the size of the set.
+    size_t size() const { return size_; }
 
-    bool empty() const {
-        return size_ == 0;
-    }
+    // Checks whether the set is empty.
+    bool empty() const { return size_ == 0; }
 
-    void insert(const T &elem) {
+    // Inserts an element if it not already in the set.
+    void insert(const T& elem) {
         bool ok = false;
         root_ = insert(root_, elem, ok);
     }
 
-    void erase(const T &elem) {
-        root_ = erase(root_, elem);
-    }
+    // Erases an element from the set.
+    void erase(const T& elem) { root_ = erase(root_, elem); }
 
+    // Returns an iterator for the first element of the set.
     iterator begin() const {
         if (min_ == nullptr) {
             return end();
@@ -182,248 +166,245 @@ public:
         return iterator(min_);
     }
 
-    iterator end() const {
-        return iterator(im_);
-    }
+    // Returns an iterator that points after the last element of the set.
+    iterator end() const { return iterator(imaginary_); }
 
-    iterator find(const T &elem) const {
-        return find(root_, elem);
-    }
+    // Returns an iterator that points at the elem or end() if there is no such.
+    iterator find(const T& elem) const { return find(root_, elem); }
 
-    iterator lower_bound(const T &elem) const {
-        return lower_bound(root_, elem);
-    }
+    // Returns an iterator that points at the first element >= elem or end() if there is no such.
+    iterator lower_bound(const T& elem) const { return lower_bound(root_, elem); }
 
-    void print() const {
-        print(root_);
-        std::cout << '\n';
-    }
-
-    int get_depth() const {
-        return geth(root_);
-    }
+    // Returns the depth of the tree.
+    size_t get_depth() const { return get_h(root_); }
 
     ~Set() {
         del(root_);
-        delete im_;
+        delete imaginary_;
     }
 
-private:
-    void print(Node *v) const {
-        if (v == nullptr) return;
-        print(v->L);
-        std::cout << v->val << ' ';
-        print(v->R);
-    }
-
-    void del(Node *v) {
+  private:
+    // Deletes a node v from the set.
+    void del(Node* v) {
         if (v == nullptr) {
             return;
         }
-        del(v->L);
-        del(v->R);
+        del(v->left);
+        del(v->right);
         delete v;
     }
 
-    Node *deep_copy(Node *a) {
+    // Deep copies the tree with root a and returns the new root.
+    Node* deep_copy(Node* a) {
         if (a == nullptr) {
             return nullptr;
         }
-        Node *v = new Node(a->val);
-        v->L = deep_copy(a->L);
-        v->R = deep_copy(a->R);
+        Node* v = new Node(a->value);
+        v->left = deep_copy(a->left);
+        v->right = deep_copy(a->right);
         return v;
     }
 
-    void fix_prev_next(Node *v, std::vector<Node *> &temp) {
+    // Fills the vector temp with valid prev and next values.
+    void fix_prev_next(Node* v, std::vector<Node*>& temp) {
         if (v == nullptr) {
             return;
         }
-        fix_prev_next(v->L, temp);
+        fix_prev_next(v->left, temp);
         temp.emplace_back(v);
-        fix_prev_next(v->R, temp);
+        fix_prev_next(v->right, temp);
     }
 
-    Node *insert(Node *v, const T &elem, bool &ok) {
+    // Inserts a node with value elem to the tree with root v.
+    Node* insert(Node* v, const T& elem, bool& ok) {
         if (v == nullptr) {
             v = new Node(elem);
-            v->next = im_;
-            if (min_ == nullptr || elem < min_->val) {
+            v->next = imaginary_;
+            if (min_ == nullptr || elem < min_->value) {
                 min_ = v;
             }
-            if (max_ == nullptr || max_->val < elem) {
+            if (max_ == nullptr || max_->value < elem) {
                 max_ = v;
-                im_->prev = max_;
+                imaginary_->prev = max_;
             }
             ++size_;
             ok = true;
             return v;
         }
-        if (elem < v->val) {
-            v->L = insert(v->L, elem, ok);
+        if (elem < v->value) {
+            v->left = insert(v->left, elem, ok);
             if (ok) {
-                v->L->next = v;
+                v->left->next = v;
                 if (v->prev != nullptr) {
-                    v->L->prev = v->prev;
-                    v->prev->next = v->L;
+                    v->left->prev = v->prev;
+                    v->prev->next = v->left;
                 }
-                v->prev = v->L;
+                v->prev = v->left;
                 ok = false;
             }
         }
-        if (v->val < elem) {
-            v->R = insert(v->R, elem, ok);
+        if (v->value < elem) {
+            v->right = insert(v->right, elem, ok);
             if (ok) {
-                v->R->prev = v;
+                v->right->prev = v;
                 if (v->next != nullptr) {
-                    v->R->next = v->next;
-                    v->next->prev = v->R;
+                    v->right->next = v->next;
+                    v->next->prev = v->right;
                 }
-                v->next = v->R;
+                v->next = v->right;
                 ok = false;
             }
         }
         return balance(v);
     }
 
-    Node *erase(Node *v, const T &elem) {
+    // Erases a node with value elem from the tree with root v.
+    Node* erase(Node* v, const T& elem) {
         if (v == nullptr) {
             return nullptr;
         }
-        if (elem < v->val) {
-            v->L = erase(v->L, elem);
-        } else if (v->val < elem) {
-            v->R = erase(v->R, elem);
+        if (elem < v->value) {
+            v->left = erase(v->left, elem);
+        } else if (v->value < elem) {
+            v->right = erase(v->right, elem);
         } else {
             if (v == min_) {
                 min_ = v->next;
             }
             if (v == max_) {
                 max_ = v->prev;
-                im_->prev = max_;
+                imaginary_->prev = max_;
             }
-            {
-                Node *A = v->prev;
-                Node *B = v->next;
-                if (A != nullptr) {
-                    A->next = B;
-                }
-                if (B != nullptr) {
-                    B->prev = A;
-                }
+
+            Node* A = v->prev;
+            Node* B = v->next;
+            if (A != nullptr) {
+                A->next = B;
             }
-            Node *A = v->L;
-            Node *B = v->R;
+            if (B != nullptr) {
+                B->prev = A;
+            }
+            A = v->left;
+            B = v->right;
             delete v;
             --size_;
             if (B == nullptr) {
                 return A;
             }
-            Node *leftest = get_min(B);
-            leftest->R = no_min(B);
-            leftest->L = A;
+            Node* leftest = get_min(B);
+            leftest->right = no_min(B);
+            leftest->left = A;
             return balance(leftest);
         }
         return balance(v);
     }
 
-    Node *get_min(Node *v) {
-        if (v == nullptr || v->L == nullptr) {
+    // Finds the node with minimal value in the tree.
+    Node* get_min(Node* v) {
+        if (v == nullptr || v->left == nullptr) {
             return v;
         } else {
-            return get_min(v->L);
+            return get_min(v->left);
         }
     }
 
-    Node *get_max(Node *v) {
-        if (v == nullptr || v->R == nullptr) {
+    // Find the node with maximal value in the tree.
+    Node* get_max(Node* v) {
+        if (v == nullptr || v->right == nullptr) {
             return v;
         } else {
-            return get_max(v->R);
+            return get_max(v->right);
         }
     }
 
-    Node *no_min(Node *v) {
+    // Returns a tree with erased min node (needed for erase method).
+    Node* no_min(Node* v) {
         if (v == nullptr) {
             return nullptr;
         }
-        if (v->L == nullptr) {
-            return v->R;
+        if (v->left == nullptr) {
+            return v->right;
         }
-        v->L = no_min(v->L);
+        v->left = no_min(v->left);
         return balance(v);
     }
 
-    iterator find(Node *v, const T &elem) const {
+    // Find method implementation.
+    iterator find(Node* v, const T& elem) const {
         if (v == nullptr) {
             return end();
         }
-        if (elem < v->val) {
-            return find(v->L, elem);
+        if (elem < v->value) {
+            return find(v->left, elem);
         }
-        if (v->val < elem) {
-            return find(v->R, elem);
+        if (v->value < elem) {
+            return find(v->right, elem);
         }
         return iterator(v);
     }
 
-    iterator lower_bound(Node *v, const T &elem) const {
+    // Lower bound method implementation.
+    iterator lower_bound(Node* v, const T& elem) const {
         if (v == nullptr) {
             return end();
         }
-        if (v->val < elem) {
-            return lower_bound(v->R, elem);
+        if (v->value < elem) {
+            return lower_bound(v->right, elem);
         }
-        auto it = lower_bound(v->L, elem);
+        auto it = lower_bound(v->left, elem);
         if (it != end()) {
             return it;
         }
-        if (!(v->val < elem)) {
+        if (!(v->value < elem)) {
             return iterator(v);
         }
         return end();
     }
 
-    Node *small_left_rotate(Node *a) {
-        Node *b = a->R;
-        a->R = b->L;
-        b->L = a;
+    // Auxiliary method for balancing the AVL tree.
+    Node* small_left_rotate(Node* a) {
+        Node* b = a->right;
+        a->right = b->left;
+        b->left = a;
         update(a);
         update(b);
         return b;
     }
 
-    Node *small_right_rotate(Node *a) {
-        Node *b = a->L;
-        a->L = b->R;
-        b->R = a;
+    // Auxiliary method for balancing the AVL tree.
+    Node* small_right_rotate(Node* a) {
+        Node* b = a->left;
+        a->left = b->right;
+        b->right = a;
         update(a);
         update(b);
         return b;
     }
 
-    Node *big_left_rotate(Node *a) {
-        a->R = small_right_rotate(a->R);
+    // Auxiliary method for balancing the AVL tree.
+    Node* big_left_rotate(Node* a) {
+        a->right = small_right_rotate(a->right);
         return small_left_rotate(a);
-
     }
 
-    Node *big_right_rotate(Node *a) {
-        a->L = small_left_rotate(a->L);
+    // Auxiliary method for balancing the AVL tree.
+    Node* big_right_rotate(Node* a) {
+        a->left = small_left_rotate(a->left);
         return small_right_rotate(a);
     }
 
-    Node *balance(Node *v) {
+    // Balancing the AVL tree.
+    Node* balance(Node* v) {
         update(v);
         if (diff(v) == -2) {
-            if (diff(v->R) > 0) {
+            if (diff(v->right) > 0) {
                 return big_left_rotate(v);
             } else {
                 return small_left_rotate(v);
             }
         }
         if (diff(v) == 2) {
-            if (diff(v->L) < 0) {
+            if (diff(v->left) < 0) {
                 return big_right_rotate(v);
             } else {
                 return small_right_rotate(v);
@@ -432,32 +413,36 @@ private:
         return v;
     }
 
-    int geth(Node *v) const {
+    // Returns the height of node v.
+    size_t get_h(Node* v) const {
         if (v == nullptr) {
             return 0;
         } else {
-            return v->h;
+            return v->height;
         }
     }
 
-    int diff(Node *v) const {
+    // Returns the height difference for children of node v that is needed for balancing the tree.
+    int diff(Node* v) const {
         if (v == nullptr) {
             return 0;
         }
-        return geth(v->L) - geth(v->R);
+        return get_h(v->left) - get_h(v->right);
     }
 
-    void update(Node *v) {
+    // Updates the height value for node v.
+    void update(Node* v) {
         if (v == nullptr) {
             return;
         }
-        int Lh = geth(v->L);
-        int Rh = geth(v->R);
-        v->h = std::max(Lh, Rh) + 1;
+        int left_h = get_h(v->left);
+        int right_h = get_h(v->right);
+        v->height = std::max(left_h, right_h) + 1;
     }
 
     size_t size_ = 0;
-    Node *root_ = nullptr;
-    Node *min_ = nullptr, *max_ = nullptr;
-    Node *im_ = nullptr;
+    Node* root_ = nullptr;
+    Node* min_ = nullptr;
+    Node* max_ = nullptr;
+    Node* imaginary_ = nullptr;
 };
